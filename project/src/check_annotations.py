410e96f71e2bd80872ec7273e5413ce2e4d275a6
Mathieu Schertenleib <schertenleib.mathieu@gmail.com>
2025-05-13 01:23:08 +0200

    Script for visualizing annotations

---
+++ project/src/check_annotations.py
@@ -0,0 +1,69 @@
+import argparse
+import json
+import os
+
+import matplotlib.pyplot as plt
+import torchvision.transforms as tf
+from torchvision.io import decode_image
+from torchvision.utils import make_grid
+from tqdm import tqdm
+
+
+def main(args: argparse.Namespace) -> None:
+
+    with open(args.annotations, "r") as f:
+        annotations = json.load(f)
+
+    # Only keep image names as keys
+    for key in list(annotations.keys()):
+        annotations[key.split(".")[0]] = annotations.pop(key)
+
+    images = {i: [] for i in range(1, 14)}
+
+    for file_name in tqdm(sorted(os.listdir(args.images))):
+        image = decode_image(os.path.join(args.images, file_name))
+
+        regions = annotations[os.path.splitext(file_name)[0]]["regions"]
+        for region in regions:
+            label = int(region["region_attributes"]["class"])
+            px = region["shape_attributes"]["all_points_x"]
+            py = region["shape_attributes"]["all_points_y"]
+            x_min = min(px)
+            y_min = min(py)
+            x_max = max(px)
+            y_max = max(py)
+            images[label].append(image[:, y_min : y_max + 1, x_min : x_max + 1])
+
+    print([len(s) for s in images.values()])
+
+    for samples in images.values():
+        height = max([image.size(1) for image in samples])
+        width = max([image.size(2) for image in samples])
+        transform = tf.Compose(
+            [tf.CenterCrop((height, width)), tf.Resize((height // 4, width // 4))]
+        )
+        for i in range(len(samples)):
+            samples[i] = transform(samples[i])
+        grid = make_grid(samples)
+
+        plt.imshow(grid.permute(1, 2, 0))
+        plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--images",
+        type=str,
+        default=os.path.join("data", "project", "train"),
+        help="folder with images",
+    )
+    parser.add_argument(
+        "--annotations",
+        type=str,
+        default=os.path.join("project", "src", "annotations.json"),
+        help="JSON file with annotations",
+    )
+    args = parser.parse_args()
+
+    main(args)
