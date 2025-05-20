b021ac5bd37eb259de3825241770c8a7f96d1946
Mathieu Schertenleib <schertenleib.mathieu@gmail.com>
2025-05-20 22:34:39 +0200

    evaluate.py: save/load predictions

--- project/src/evaluate.py
+++ project/src/evaluate.py
@@ -5,6 +5,7 @@ import os
 from typing import Sequence
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
@@ -79,8 +80,6 @@ def main(args: argparse.Namespace) -> None:
     model.load_state_dict(torch.load(checkpoint, map_location="cpu"))
     model = model.to(device).eval()
 
-    labels_df = pd.read_csv(args.labels, index_col="id")
-
     train_images = [f for f in os.listdir(args.train_images) if f not in cfg.val_images]
     train_images = [os.path.join(args.train_images, f) for f in train_images]
     if args.test_images is not None:
@@ -88,13 +87,92 @@ def main(args: argparse.Namespace) -> None:
     else:
         test_images = [os.path.join(args.train_images, f) for f in cfg.val_images]
 
+    cfg = dataclasses.replace(cfg, patch_stride=4)
+
+    if args.load_pred:
+        image_names = [os.path.splitext(os.path.basename(f))[0] for f in test_images]
+        load_dir = os.path.join(log_dir, "predictions")
+        files_to_load = [
+            os.path.join(load_dir, f)
+            for f in os.listdir(load_dir)
+            if os.path.splitext(f)[0] in image_names
+        ]
+        stitched_preds_list = [torch.from_numpy(np.load(f)) for f in files_to_load]
+
+    else:
+        test_dataset = PatchDataset(
+            cfg,
+            test_images,
+            annotations_file=None,
+            transform=False,
+            mean=cfg.image_mean,
+            std=cfg.image_std,
+        )
+        image_names = test_dataset.image_names
+        test_loader = torch.utils.data.DataLoader(
+            test_dataset, batch_size=cfg.batch_size * 4, shuffle=False
+        )
+
+        num_images, _, image_height, image_width = test_dataset.images.size()
+        patch_rows = (image_height - cfg.patch_size) // cfg.patch_stride + 1
+        patch_cols = (image_width - cfg.patch_size) // cfg.patch_stride + 1
+        assert num_images * patch_rows * patch_cols == len(test_dataset)
+
+        patch_count = 0
+        preds_list = []
+        stitched_preds_list = []
+
+        with torch.no_grad():
+            for patch in tqdm(test_loader):
+                patch = patch.to(device)
+                pred = model(patch)
+                pred = torch.softmax(pred, dim=-1)
+                preds_list.append(pred)
+                patch_count += patch.size(0)
+
+                if patch_count >= patch_rows * patch_cols:
+                    # The number of patches in an image is likely not a multiple of the batch size,
+                    # so we have to correctly split the batches that fall on the boundary from one
+                    # image to the next.
+                    flat_preds = torch.concat(preds_list, dim=0)[
+                        : patch_rows * patch_cols, ...
+                    ].cpu()
+                    num_extras = patch_count - patch_rows * patch_cols
+                    preds_list = [pred[-num_extras:, ...]]
+                    patch_count = num_extras
+
+                    # shape (C, patch_rows, patch_cols)
+                    preds = flat_preds.unflatten(0, (patch_rows, patch_cols)).permute(2, 0, 1)
+
+                    stitched_pred = stitch(
+                        preds.unsqueeze(0),
+                        (image_height, image_width),
+                        patch_size=cfg.patch_size,
+                        stride=cfg.patch_stride,
+                    ).squeeze(0)
+                    stitched_preds_list.append(stitched_pred)
+
+    if args.save_pred:
+        save_dir = os.path.join(log_dir, "predictions")
+        os.makedirs(save_dir, exist_ok=True)
+        for image_name, stitched_pred in zip(image_names, stitched_preds_list):
+            np.save(os.path.join(save_dir, f"{image_name}.npy"), stitched_pred.numpy())
+
+    #
+    #
+    #  Count prediction
+    #
+    #
+
+    labels_df = pd.read_csv(args.labels, index_col="id")
+    # FIXME: don't load the full dataset just for the mask areas
     train_dataset = PatchDataset(
         cfg,
         train_images,
         annotations_file=args.annotations,
         transform=False,
     )
-
+    # FIXME: use counts_from_csv
     object_counts = torch.zeros(13, dtype=torch.long)
     pixel_counts = torch.zeros(13, dtype=torch.long)
     for i, image_name in enumerate(train_dataset.image_names):
@@ -107,82 +185,26 @@ def main(args: argparse.Namespace) -> None:
         torch.float32
     )
 
-    cfg = dataclasses.replace(cfg, patch_stride=4)
-    test_dataset = PatchDataset(
-        cfg,
-        test_images,
-        annotations_file=args.annotations if args.test_images is None else None,
-        transform=False,
-        mean=cfg.image_mean,
-        std=cfg.image_std,
-    )
-    test_loader = torch.utils.data.DataLoader(
-        test_dataset, batch_size=cfg.batch_size * 4, shuffle=False
-    )
-
-    num_images, _, image_height, image_width = test_dataset.images.size()
-    patch_rows = (image_height - cfg.patch_size) // cfg.patch_stride + 1
-    patch_cols = (image_width - cfg.patch_size) // cfg.patch_stride + 1
-    assert num_images * patch_rows * patch_cols == len(test_dataset)
-
-    patch_count = 0
-    preds_list = []
-    stitched_preds_list = []
-
-    with torch.no_grad():
-        for data in tqdm(test_loader):
-            if isinstance(data, list):
-                patch, label = data
-            else:
-                patch = data
-                label = None
-
-            patch = patch.to(device)
-            pred = model(patch)
-            pred = torch.softmax(pred, dim=-1)
-            preds_list.append(pred)
-            patch_count += patch.size(0)
-
-            if patch_count >= patch_rows * patch_cols:
-                # The number of patches in an image is likely not a multiple of the batch size,
-                # so we have to correctly split the batches that fall on the boundary from one
-                # image to the next.
-                flat_preds = torch.concat(preds_list, dim=0)[: patch_rows * patch_cols, ...].cpu()
-                num_extras = patch_count - patch_rows * patch_cols
-                preds_list = [pred[-num_extras:, ...]]
-                patch_count = num_extras
-
-                # shape (C, patch_rows, patch_cols)
-                preds = flat_preds.unflatten(0, (patch_rows, patch_cols)).permute(2, 0, 1)
-                print(preds.size())
-
-                stitched_preds = stitch(
-                    preds.unsqueeze(0),
-                    (image_height, image_width),
-                    patch_size=cfg.patch_size,
-                    stride=cfg.patch_stride,
-                ).squeeze(0)
-                stitched_preds_list.append(stitched_preds)
-
-                """# size (H, W)
-                class_prob, pred_class = torch.max(pred_probs, dim=0)
-
-                pred_pixel_counts = pred_class.flatten().bincount(minlength=14)[1:]
-                pred_count = torch.round(pred_pixel_counts.to(torch.float32) / object_sizes).to(
-                    torch.long
-                )
-                pred_counts.append(pred_count)"""
-
-    exit()
+    pred_counts = []
+    for stitched_pred in stitched_preds_list:
+        # size (H, W)
+        class_prob, pred_class = torch.max(stitched_pred, dim=0)
+        pred_pixel_counts = pred_class.flatten().bincount(minlength=14)[1:]
+        pred_count = torch.round(pred_pixel_counts.to(torch.float32) / object_sizes).to(torch.long)
+        pred_counts.append(pred_count)
 
     pred_counts = torch.stack(pred_counts, dim=0)
-    counts_to_csv(pred_counts, image_names, "submission.csv")
+    counts_to_csv(pred_counts, image_names, args.submission)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--checkpoint", type=str, required=True, help="checkpoint to load")
-    parser.add_argument("--cpu", action="store_true", help="force running on the CPU")
+    parser.add_argument(
+        "--checkpoint",
+        type=str,
+        default=os.path.join("checkpoints", "WRN-16-4", "seed_45"),
+        help="Checkpoint to load",
+    )
     parser.add_argument(
         "--train_images",
         type=str,
@@ -208,6 +230,15 @@ if __name__ == "__main__":
         help="Test images. If specified, evaluates the model on this testing set. "
         "Otherwise, evaluates the model on its validation split of the training set.",
     )
+    parser.add_argument(
+        "--submission",
+        type=str,
+        default=os.path.join("project", "submission.csv"),
+        help="Submission file to create",
+    )
+    parser.add_argument("--save_pred", action="store_true", help="Save model predictions")
+    parser.add_argument("--load_pred", action="store_true", help="Load saved model predictions")
+    parser.add_argument("--cpu", action="store_true", help="Force running on the CPU")
     args = parser.parse_args()
 
     main(args)
